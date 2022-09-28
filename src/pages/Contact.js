a9b02b8525a32c34a52992aa3c0985c073cf1251
Jacksonckc <jackson858216047@gmail.com>
2022-09-28 13:02:05 -0600

    adding more css for contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,11 @@
 const Contact = () => {
-  return <div className='contact'>Contact Me</div>;
+  return (
+    <div className='contact'>
+      <div>Talia Chan</div>
+      <div>Number: 9999999999</div>
+      <div>Email: taliavh13@icloud.com</div>
+    </div>
+  );
 };
 
 export default Contact;
